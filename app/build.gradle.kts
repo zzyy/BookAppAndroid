6349c735611be35fb1288a32d18b62fe73fdf26b
yiyan <yunzou@thoughtworks.com>
2024-04-28 12:49:58 +0800

    [Arch] add hilt for dependency inject

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
-
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.dagger.hilt.android")
+    id("kotlin-parcelize")
 }
 
 android {
@@ -66,6 +67,9 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/thoughtworks/hsbc/bookinfoapp/App.kt
+++ app/src/main/java/com/thoughtworks/hsbc/bookinfoapp/App.kt
@@ -1,6 +1,8 @@
 package com.thoughtworks.hsbc.bookinfoapp
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class App : Application() {
 }
